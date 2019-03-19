e22f42108154d1d9f305c869748b65bd5084079d
RU20017707 <dmitrymokin@mail.ru>
2019-03-19 17:26:13 +0300

    Task1. Sample service.

---
+++ src/main/java/net/dev/resume/service/NameService.java
@@ -0,0 +1,12 @@
+package net.dev.resume.service;
+
+public class NameService {
+
+    public static NameService getInstance(){
+        return new NameService();
+    }
+
+    public String convertName(String name) {
+        return name.toUpperCase();
+    }
+}
